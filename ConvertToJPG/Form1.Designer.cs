6311aa932324defe6138ff3ccb14d075d62d69a9
AndriSetyawan2014 <andri.setyawan2014@gmail.com>
2020-01-09 00:00:02 +0700

    - Add Convert Image Type - Add Release build

--- ConvertToJPG/Form1.Designer.cs
+++ ConvertToJPG/Form1.Designer.cs
@@ -29,61 +29,93 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBoxBMP = new System.Windows.Forms.CheckBox();
+            this.checkBoxPNG = new System.Windows.Forms.CheckBox();
+            this.buttonConvertToJPG = new System.Windows.Forms.Button();
+            this.labelWarning = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.checkBox2);
-            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.checkBoxBMP);
+            this.groupBox1.Controls.Add(this.checkBoxPNG);
             this.groupBox1.Location = new System.Drawing.Point(45, 29);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(487, 147);
+            this.groupBox1.Size = new System.Drawing.Size(294, 79);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Image Type";
             // 
-            // checkBox1
+            // checkBoxBMP
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(6, 16);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(52, 17);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = ".PNG";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBoxBMP.AutoSize = true;
+            this.checkBoxBMP.Location = new System.Drawing.Point(6, 48);
+            this.checkBoxBMP.Name = "checkBoxBMP";
+            this.checkBoxBMP.Size = new System.Drawing.Size(52, 17);
+            this.checkBoxBMP.TabIndex = 1;
+            this.checkBoxBMP.Text = ".BMP";
+            this.checkBoxBMP.UseVisualStyleBackColor = true;
+            this.checkBoxBMP.CheckedChanged += new System.EventHandler(this.checkBoxBMP_CheckedChanged);
             // 
-            // checkBox2
+            // checkBoxPNG
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(6, 39);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(52, 17);
-            this.checkBox2.TabIndex = 1;
-            this.checkBox2.Text = ".BMP";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBoxPNG.AutoSize = true;
+            this.checkBoxPNG.Location = new System.Drawing.Point(6, 25);
+            this.checkBoxPNG.Name = "checkBoxPNG";
+            this.checkBoxPNG.Size = new System.Drawing.Size(52, 17);
+            this.checkBoxPNG.TabIndex = 0;
+            this.checkBoxPNG.Text = ".PNG";
+            this.checkBoxPNG.UseVisualStyleBackColor = true;
+            this.checkBoxPNG.CheckedChanged += new System.EventHandler(this.checkBoxPNG_CheckedChanged);
+            // 
+            // buttonConvertToJPG
+            // 
+            this.buttonConvertToJPG.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonConvertToJPG.Location = new System.Drawing.Point(192, 131);
+            this.buttonConvertToJPG.Name = "buttonConvertToJPG";
+            this.buttonConvertToJPG.Size = new System.Drawing.Size(147, 47);
+            this.buttonConvertToJPG.TabIndex = 1;
+            this.buttonConvertToJPG.Text = "Convert To JPG";
+            this.buttonConvertToJPG.UseVisualStyleBackColor = true;
+            this.buttonConvertToJPG.Click += new System.EventHandler(this.buttonConvertToJPG_Click);
+            // 
+            // labelWarning
+            // 
+            this.labelWarning.AutoSize = true;
+            this.labelWarning.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWarning.Location = new System.Drawing.Point(42, 189);
+            this.labelWarning.Name = "labelWarning";
+            this.labelWarning.Size = new System.Drawing.Size(98, 13);
+            this.labelWarning.TabIndex = 2;
+            this.labelWarning.Text = "label WARNING";
+            this.labelWarning.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(669, 302);
+            this.ClientSize = new System.Drawing.Size(377, 211);
+            this.Controls.Add(this.labelWarning);
+            this.Controls.Add(this.buttonConvertToJPG);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Convert To JPG";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBoxBMP;
+        private System.Windows.Forms.CheckBox checkBoxPNG;
+        private System.Windows.Forms.Button buttonConvertToJPG;
+        private System.Windows.Forms.Label labelWarning;
     }
 }
 
--- ConvertToJPG/Form1.cs
+++ ConvertToJPG/Form1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D; // to convert type
+using System.Drawing.Imaging; // to convert type
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,70 @@ namespace ConvertToJPG
 {
     public partial class Form1 : Form
     {
+        bool convertFromPNG = false;
+        bool convertFromBMP = false;
+        int countFile = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonConvertToJPG_Click(object sender, EventArgs e)
+        {
+            convertFromPNG = checkBoxPNG.Checked;
+            convertFromBMP = checkBoxBMP.Checked;
+            countFile = 0;
+
+            if (!convertFromPNG && !convertFromBMP)
+            {
+                setTextWarning("Please select image type ...");                
+            }
+            else
+            {
+                string CurrentFolder = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+                convertToJPG_Func(CurrentFolder);
+            }            
+        }
+
+        public void convertToJPG_Func(string folder)
+        {            
+            foreach (string file in System.IO.Directory.GetFiles(folder))
+            {
+                if(convertFromPNG) convertImageType(file, ".png", ".jpg");
+                if(convertFromBMP) convertImageType(file, ".bmp", ".jpg");                
+            }
+            setTextWarning("Convert "+ countFile +" files Complete...");
+        }
+
+        public void convertImageType(string fileSrc, string originalType, string resultType)
+        {
+            string extension = System.IO.Path.GetExtension(fileSrc); // get file extention 
+            if (extension.ToLower() == originalType)
+            {
+                string name = System.IO.Path.GetFileNameWithoutExtension(fileSrc);
+                string path = System.IO.Path.GetDirectoryName(fileSrc);
+                Image resultImage = Image.FromFile(fileSrc);
+                resultImage.Save(path + @"/" + name + resultType, System.Drawing.Imaging.ImageFormat.Jpeg);
+                resultImage.Dispose();
+                countFile++;
+            }
+        }
+
+        public void setTextWarning (string textWarning)
+        {
+            labelWarning.Visible = true;
+            labelWarning.Text = textWarning;
+        }
+
+        private void checkBoxPNG_CheckedChanged(object sender, EventArgs e)
+        {
+            labelWarning.Visible = false;
+        }
+
+        private void checkBoxBMP_CheckedChanged(object sender, EventArgs e)
+        {
+            labelWarning.Visible = false;
         }
     }
 }
